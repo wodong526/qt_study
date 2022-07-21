3427e6ce66b116c36d095841dd1b93f0143913c6
woDong <wodong526@dingtalk.com>
2022-07-21 17:34:44 +0800

    跟新qt模板和删除空文件

--- maya_window_Template.py
+++ maya_window_Template.py
@@ -1,6 +1,3 @@
-import pstats
-
-from sqlalchemy import true
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 from PySide2 import QtGui
@@ -14,11 +11,11 @@ def maya_main_window():
     return wrapInstance(int(main_window_ptr), QtWidgets.QWidget)
 
 class WindowClassName(QtWidgets.QDialog):
-    def __init__(self, parent = maya_main_window()) -> None:
-        super(SpinBoxDialog, self).__init__(parent)
+    def __init__(self, parent = maya_main_window()):
+        super(WindowClassName, self).__init__(parent)
 
         self.setWindowTitle('窗口抬头')
-        if mc.about(ntOs = True):#判断系统类型
+        if mc.about(ntOS = True):#判断系统类型
             self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)#删除窗口上的帮助按钮
         elif mc.about(macOS = True):
             self.setWindowFlags(QtCore.Qt.Tool)
@@ -27,13 +24,13 @@ class WindowClassName(QtWidgets.QDialog):
         self.create_layout()
         self.create_connections()
     
-    def create_widgets(self) -> None:
+    def create_widgets(self):
         pass
     
-    def create_layout(self) -> None:
+    def create_layout(self):
         pass
     
-    def create_connections(self) -> None:
+    def create_connections(self):
         pass
 
 if __name__ == '__main__':
